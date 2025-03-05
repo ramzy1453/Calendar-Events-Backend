9e0a1469ae924abca017dec65a8a3134c6a83b5b
ramzy1453 <kemmounramzy93@gmail.com>
2025-03-05 03:00:15 +0100

    feat: implemented push web notifs for create event

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,17 +2,9 @@ import { createClient } from "redis";
 import { REDIS_URL } from "./env";
 
 export const redisClient = createClient({ url: REDIS_URL });
-export const redisPublisher = createClient({ url: REDIS_URL });
-export const redisSubscriber = createClient({ url: REDIS_URL });
 
-[redisClient, redisPublisher, redisSubscriber].forEach((client) => {
-  client.on("error", (err) => console.error("❌ Redis Error:", err));
-});
+redisClient.on("error", (err) => console.error("❌ Redis Error:", err));
 
-Promise.all([
-  redisClient.connect(),
-  redisPublisher.connect(),
-  redisSubscriber.connect(),
-]).then(() => console.log("✅ Redis connected"));
+redisClient.connect().then(() => console.log("✅ Redis connected"));
 
 export default redisClient;
--- src/routes/notifications.route.ts
+++ src/routes/notifications.route.ts
@@ -1,27 +1,37 @@
 import express from "express";
-import { redisSubscriber } from "../config/redis";
+import { createClient } from "redis";
 
 const router = express.Router();
 const CHANNELS = ["new_event"];
 
-router.get("/notifications", async (req, res) => {
+router.get("/", async (req, res) => {
   res.setHeader("Content-Type", "text/event-stream");
   res.setHeader("Cache-Control", "no-cache");
   res.setHeader("Connection", "keep-alive");
 
+  const subscriber = createClient();
+  await subscriber.connect();
+
   for (const channel of CHANNELS) {
-    await redisSubscriber.subscribe(channel, (message, channel) => {
+    await subscriber.subscribe(channel, (message, channel) => {
       try {
         console.log(`📥 Received ${channel} from Redis`);
-        res.write(`data: ${JSON.parse(message)}\n\n`);
+        const data = JSON.stringify({
+          type: channel,
+          content: message,
+        });
+        res.write(`data: ${data}\n\n`);
       } catch (error) {
         console.error(`❌ Error parsing Redis message from ${channel}:`, error);
       }
     });
   }
 
-  req.on("close", () => {
-    redisSubscriber.unsubscribe("new_event");
+  req.on("close", async () => {
+    for (const channel of CHANNELS) {
+      await subscriber.unsubscribe(channel);
+    }
+    await subscriber.quit();
     console.log("🔌 SSE connection closed");
   });
 });
